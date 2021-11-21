e38481e7de2e9a99d75bad5b452447af89fccb77
Facundo Besse <facundobesse@gmail.com>
2021-11-21 00:08:45 -0300

    se empieza con el modulo de modificacion

--- src/main/java/com/FacuEgg/libreria/controladores/AutorController.java
+++ src/main/java/com/FacuEgg/libreria/controladores/AutorController.java
@@ -1,4 +1,3 @@
-
 package com.FacuEgg.libreria.controladores;
 
 import com.FacuEgg.libreria.entidades.Autor;
@@ -9,40 +8,56 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/autor")
 public class AutorController {
-    
+
     @Autowired
     private AutorServicio autorServicio;
-    
+
     @GetMapping("/")
-    public String autor(ModelMap modelo){
+    public String autor(ModelMap modelo) {
         List<Autor> listAutor = autorServicio.listar();
-        modelo.addAttribute("autores",listAutor);
+        modelo.addAttribute("autores", listAutor);
         return "autor";
     }
-    
+
     @GetMapping("/carga")
-    public String formulario(){
+    public String formulario() {
         return "autor_form";
     }
-    
+
     @PostMapping("/carga")
-    public String carga(ModelMap modelo, @RequestParam String nombre){
+    public String carga(ModelMap modelo, @RequestParam String nombre) {
         try {
             autorServicio.registrar(nombre);
-            modelo.put("exito","Registro Exitoso");
+            modelo.put("exito", "Registro Exitoso");
         } catch (Exception e) {
             modelo.put("error", e.getMessage());
             return "autor_form";
         }
-        
-        return "index";
+        return "autor_form";
     }
     
-    
+    @GetMapping("/modificar")
+    public String formModificar(){
+        return"autor_modificar";
+    }
+
+    @PutMapping("/modificar")
+    public String modificar(ModelMap modelo, @RequestParam String id, @RequestParam String nombre) {
+        try {
+            autorServicio.modificar(id, nombre);
+            modelo.put("exito", "se modifico correctamente");
+        } catch (Exception e) {
+            modelo.put("error", e.getMessage());
+            return "";
+        }
+        return "";
+    }
+
 }
